d25ba2a6b41dbe00b1e26be2583b104385e1a098
KolioDivaka <nikipavlov2005@gmail.com>
2025-02-19 20:58:50 +0200

    Sign up page added
    Add the sign up page and some touches to the Log in.

--- src/components/Sign_up_Page/Sign_up_Page.tsx
+++ src/components/Sign_up_Page/Sign_up_Page.tsx
@@ -1,10 +1,23 @@
+import { Link } from 'react-router'; 
+import "./Sign_up_Page.css";
+
 export default function Sign_up_Page() {
-
     return (
-        <div >
-            <h1>Sign up</h1>
+        <div className="sign-up-page">
+            <div className="sign-up--container">
+                <form className="sign-up-form">
+                    <h1 className="sign-up-form--title">Sign Up</h1>
+                    <div className="sign-up-form--info--container">
+                        <input type="text" className="sign-up-form--input" placeholder="Username" />
+                        <input type="email" className="sign-up-form--input" placeholder="Email" />
+                        <input type="password" className="sign-up-form--input" placeholder="Password" />
+                        <input type="password" className="sign-up-form--input" placeholder="Confirm Password" />
+                    </div>
+                    <button className="sign-up-form--button">Sign Up</button>
+                    <p className='login--text'>Have an account?</p>
+                    <Link to="/Profile" className="login--link">Log in</Link>
+                </form>
+            </div>
         </div>
-
-
-    )
+    );
 }
