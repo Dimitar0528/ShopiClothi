859b8c24027cbeca0d0b4e67194ad3e5f379fec5
Dimitar0528 <dimityr_dukov@abv.bg>
2025-03-17 17:22:27 +0200

    add about us and contact us components

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,20 @@ import Log_in_Page from "./components/authentication/Log_in_Page/Log_in_Page";
 import Sign_up_Page from "./components/authentication/Sign_up_Page/Sign_up_Page";
 import ScrollToTopButton from "./components/ui/ScrollToTopButton/ScrollToTopButton";
 import ShoppingBag from "./components/ShoppingBag/ShoppingBag";
-import ProductDetail from "./components/ProductDetail/ProductDetail";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
+import AboutUs from "./components/AboutUs/AboutUs";
+import ContactUs from "./components/ContactUs/ContactUs";
 
 function App() {
   useDynamicTitle();
   const location = useLocation();
 
   // Define the paths where we don't want to show the Newsletter
-  const hideNewsletterRoutes = ["/profile", "/sign_up"];
+  const hideNewsletterRoutes = [
+    "/profile",
+    "/sign_up",
+    "/shopping-bag"
+  ];
 
   const shouldHideNewsletter = () => {
     return hideNewsletterRoutes.some((route) =>
@@ -30,22 +36,24 @@ function App() {
     <>
       <ScrollToTopButton />
       <Header />
-      <div id="main-content">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/profile" element={<Log_in_Page />} />
-          <Route path="/sign-up" element={<Sign_up_Page />} />
-          <Route path="/shopping-bag" element={<ShoppingBag />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-of-service" element={<TermsOfService />} />
-          <Route path="*" element={<Navigate to={"/"} />} />
-        </Routes>
+        <div id="main-content">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/profile" element={<Log_in_Page />} />
+            <Route path="/sign-up" element={<Sign_up_Page />} />
+            <Route path="/shopping-bag" element={<ShoppingBag />} />
+            <Route path="/products/:id" element={<ProductDetails />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/contact-us" element={<ContactUs />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
+            <Route path="*" element={<Navigate to={"/"} />} />
+          </Routes>
 
-        {/* Render Newsletter only if the current path is NOT in the hideNewsletterRoutes list */}
-        {!shouldHideNewsletter() && <Newsletter />}
-      </div>
+          {/* Render Newsletter only if the current path is NOT in the hideNewsletterRoutes list */}
+          {!shouldHideNewsletter() && <Newsletter />}
+        </div>
       <Footer />
     </>
   );
